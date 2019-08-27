893a404377c15c441d207fcf79e924e32be675fe
ahenteti <ahmed.henteti@capgemini.com>
2019-08-27 22:42:55 +0200

    move text-overflow-ellipsis-demo to css directory

---
+++ java/joda-time-demo/src/main/java/io/github/ahenteti/java/App.java
@@ -0,0 +1,13 @@
+package io.github.ahenteti.java;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
