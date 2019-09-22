b3540183cc292aac97c337e3325cecbe2ca39ab7
ahenteti <ahmad.henteti@gmail.com>
2019-09-22 23:38:18 +0200

    update facelets-templating-system project

---
+++ javaee/jsf-websocket-demo/src/main/java/io/github/ahenteti/java/HelloWorldMBean.java
@@ -0,0 +1,17 @@
+package io.github.ahenteti.java;
+
+import java.io.Serializable;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+
+import lombok.Data;
+
+@Data
+@Named
+@SessionScoped
+public class HelloWorldMBean implements Serializable {
+
+    private static final long serialVersionUID = 5443351151396868724L;
+    private String message = "Hello World JSF!";
+
+}
